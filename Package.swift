e9e6d6f9720583c79d9dfc09bd09965c5a1bd5ae
Chebakov <chebakov@mts.ru>
2024-06-05 15:17:32 +0300

    Update bad name

--- Package.swift
+++ Package.swift
@@ -4,17 +4,17 @@
 import PackageDescription
 
 let package = Package(
-    name: "EpandingButton",
+    name: "ExpandingButton",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "EpandingButton",
-            targets: ["EpandingButton"]),
+            name: "ExpandingButton",
+            targets: ["ExpandingButton"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "EpandingButton")
+            name: "ExpandingButton")
     ]
 )
